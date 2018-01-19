e30709647a8f00c6b566e6972929865bcea98b90
SButler <sm291778@dal.ca>
2018-01-18 23:52:14 -0400

    Added Functionality
    Added Button and input box
    typed msg will display on the screen when button is pressed!

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        final EditText mEdit = (EditText) findViewById(R.id.editText3);
+        final Button button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                final TextView textView = findViewById(R.id.TextView1);
+                textView.setText(mEdit.getText().toString());
+
+            }
+        });
     }
 }
